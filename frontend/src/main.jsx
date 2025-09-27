1c55dbb75473d7b9903c905573983ab5d5d13d5a
Sai G <gaddamsaiom963@gmail.com>
2025-09-27 15:39:48 +0530

    fix(frontend): use existing PrimeReact theme (lara-light-cyan) instead of removed saga-blue

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App'
 // PrimeReact styles
-import 'primereact/resources/themes/saga-blue/theme.css'
+import 'primereact/resources/themes/lara-light-cyan/theme.css'
 import 'primereact/resources/primereact.min.css'
 import 'primeicons/primeicons.css'
 // Optional utility CSS
